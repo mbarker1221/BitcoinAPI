46a34030bede2edd34cac9d96b1fb32e7160331e
michelle barker <mbarker1221@gmail.com>
2018-01-11 04:26:25 -0500

    bug fixes and style changes

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-const crypto = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,ETH,QTUM,GEO,BTG,ETC,XRP,SWIFT,OMNI,IOT,XEM,DASH,BCH,ZEC&tsyms=USD&extraParams=your_app_name";
-const starts = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD";
-const calcExchange = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,GBP,CNY,JPY,EUR,TWD,JPY,RUB,CHF,CAD,AUD,SGD";
-const news = "https://newsapi.org/v2/top-headlines?q=bitcoin&sortBy=popularity&language=en&apiKey=29b9b388e7c142329c131bb139ff5429"
+const baseURL = "https://min-api.cryptocompare.com/data/"
+const cryptoURL = baseURL + "pricemulti?fsyms=BTC,LTC,ETH,QTUM,GEO,BTG,ETC,XRP,SWIFT,OMNI,IOT,XEM,DASH,BCH,ZEC&tsyms=USD&extraParams=your_app_name";
+const startsURL = baseURL + "pricemultifull?fsyms=BTC&tsyms=USD";
+const calcExchangeURL = baseURL + "price?fsym=BTC&tsyms=USD,GBP,CNY,JPY,EUR,TWD,JPY,RUB,CHF,CAD,AUD,SGD";
+const newsURL = "https://newsapi.org/v2/top-headlines?q=bitcoin&sortBy=popularity&language=en&apiKey=29b9b388e7c142329c131bb139ff5429";
 
 function handleNavigation() {
    // select nav elements on"click'
-   $('nav a').click(function(event) {
+   $("nav a").click(function(event) {
       //prevent default
       event.preventDefault();
       //current page is current target using data method 
-      const currentPage = $(this).data('page');
+      const currentPage = $(this).data("page");
       //hiding elements using class
-      $('.page').hide();
+      $(".page").hide();
       //display current page
       $(`.${currentPage}`).show();
    });
 }
-
+//navigation slide 
 function openNav() {
    document.getElementById("mySidenav").style.width = "15%";
    document.getElementById("main").style.marginLeft = "15%";
 }
 
-function closeNav() {
-   document.getElementById("mySidenav").style.width = "0";
-   document.getElementById("main").style.marginLeft = "0";
-}
-
 // Clear form values
 function resetBitcoinExchange() {
    $("#bitcoinAmount").val('');
-
-   //show and hide
    $("#btnCalculateRates").show();
-   $("#btnResetRates").hide();
-   $('#press').show();
 }
-
+//show landing page
 function showStartPage() {
-   $('.page').hide();
-   $('.start-page').show();
+   $(".page").hide();
+   $(".start-page").show();
 }
 
 function setEventListeners() {
    handleNavigation();
 }
-
+//get data for landing page from api and display
 function fetchOnScreenLoad() {
-   $.getJSON(starts, function(data) {
+   $.getJSON(startsURL, function(data) {
       showFirstValueOnScreenLoad(data);
    });
 }
@@ -71,9 +63,9 @@ function showFirstValueOnScreenLoad(results) {
    $(`#marketCap`).text(marketCap.toFixed(1));
 }
 
-// Get cryptocurrency exchange rates
+// Get cryptocurrency exchange rates from api and display 
 function fetchCryptoExchangeRates() {
-   $.getJSON(crypto, function(data) {
+   $.getJSON(cryptoURL, function(data) {
       showCryptoExchangeRates(data);
    });
 }
@@ -106,18 +98,18 @@ function showCryptoExchangeRates(results) {
    let xrp = results.XRP.USD;
    $(`#XRP`).text("$" + xrp);
 }
-
+//get and display exchange rate data using user-supplied params
 function fetchBitcoinExchangeData() {
-   const amount = $('input[name=bitcoinAmount]');
+   const amount = $("input[name=bitcoinAmount]");
    const amountValue = amount.val();
 
-   $.getJSON(calcExchange, function(data) {
+   $.getJSON(calcExchangeURL, function(data) {
       showBitcoinExchangeData(data)
    })
 }
 
 function showBitcoinExchangeData(results) {
-   const amount = $('input[name=bitcoinAmount]').val();
+   const amount = $("input[name=bitcoinAmount]").val();
    let usd = results.USD;
    let usd2 = (usd * amount);
    $(`#USD`).text("$" + usd2.toFixed(2));
@@ -138,7 +130,7 @@ function showBitcoinExchangeData(results) {
    $(`#TWD`).text("$" + twd2.toFixed(2));
    let rub = results.RUB;
    let rub2 = (rub * amount);
-   $(`#RUB`).text("₽ " + rub2.toFixed(2));
+   $(`#RUB`).text("₽" + rub2.toFixed(2));
    let chf = results.CHF;
    let chf2 = (chf * amount);
    $(`#CHF`).text("$" + chf2.toFixed(2));
@@ -147,53 +139,50 @@ function showBitcoinExchangeData(results) {
    $(`#AUD`).text("$" + aud2.toFixed(2));
    renderExchangeResults()
 }
-
+//clear out value of input box
 function renderExchangeResults() {
-   $('#bitcoinAmount').val("");
-   $('#btnCalculateRates').hide;
-   $('#btnResetRates').show;
-   showBitcoinExchangeData(data);
+   $("#bitcoinAmount").val("");
 }
 
 function showNewsPage() {
-   $('.start-page').hide();
-   $('.history-page').hide();
-   $('.crypto-page').hide();
-   $('.calc-page').hide();
-   $('.news-page').show();
+   $(".start-page").hide();
+   $(".history-page").hide();
+   $(".crypto-page").hide();
+   $(".calc-page").hide();
+   $(".news-page").show();
    getNews();
 }
-
+//get news stories from API
 function getNews() {
-   $.getJSON(news, function(data) {
+   $.getJSON(newsURL, function(data) {
       showNews(data);
    });
 }
-
+//render news thumbnails to page
 function showNews(results) {
    let article = `
    
    <a class="link" href="${results.articles[0].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[0].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[0].url}" target="_blank">${results.articles[0].title}<alt="${results.articles[0].description}"></a><br /><br /><br />
+   <a class="title" href="${results.articles[0].urlToImage}" target="_blank">${results.articles[0].title}<alt="${results.articles[0].description}"></a><br /><br /><br />
    
    
    <a class="link" href="${results.articles[1].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[1].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[1].url}" target="_blank">${results.articles[1].title} <alt="${results.articles[1].description}"></a><br /><br /><br />
+   <a class="title" href="${results.articles[1].urlToImage}" target="_blank">${results.articles[1].title} <alt="${results.articles[1].description}"></a><br /><br /><br />
    
  
    <a class="link" href="${results.articles[2].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[2].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[2].url}" target="_blank">${results.articles[2].title}<alt="${results.articles[2].description}"></a><br /><br /><br />
+   <a class="title" href="${results.articles[2].urlToImage}" target="_blank">${results.articles[2].title}<alt="${results.articles[2].description}"></a><br /><br /><br />
  
  
    <a class="link" href="${results.articles[3].urlToImage}" target="_blank"><img class="thumb" src="${results.articles[3].urlToImage}"></a><br />
    
-   <a class="title" href="${results.articles[3].url}" target="_blank">${results.articles[3].title}<alt="${results.articles[3].description}"></a><br /><br />
+   <a class="title" href="${results.articles[3].urlToImage}" target="_blank">${results.articles[3].title}<alt="${results.articles[3].description}"></a><br /><br />
    
    `;
-   $('.stories').html(article);
+   $(".stories").html(article);
 }
 // On load event:
 $(() => {
